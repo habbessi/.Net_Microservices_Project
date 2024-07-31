da9afdf2222cb1657897759d63193c5604ab6083
dell <habbessi.houssem10@gmail.com.com>
2024-07-31 14:28:55 +0200

    Ajoutez des fichiers projet.

---
+++ Mango.MessageBus/MessageBus.cs
@@ -0,0 +1,32 @@
+﻿using Azure.Messaging.ServiceBus;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mango.MessageBus
+{
+    public class MessageBus : IMessageBus
+    {
+
+        private string connectionString = "Endpoint=sb://mangomicro.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=BdosXF9+doQgpiP16DDvfQ0LyAE7MPNq9+ASbMvXJCo=";
+        public async Task PublishMessage(object message, string topic_queue_Name)
+        {
+            await using var client = new ServiceBusClient(connectionString);
+
+            ServiceBusSender sender = client.CreateSender(topic_queue_Name);
+
+            var jsonMessage = JsonConvert.SerializeObject(message);
+            ServiceBusMessage finalMessage = new ServiceBusMessage(Encoding
+                .UTF8.GetBytes(jsonMessage))
+            {
+                CorrelationId = Guid.NewGuid().ToString(),
+            };
+
+            await sender.SendMessageAsync(finalMessage);
+            await client.DisposeAsync();
+        }
+    }
+}
